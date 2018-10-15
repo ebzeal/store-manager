5366a3052e6d4bd8af2cf983678115df527c6be0
ebzeal <ebenezerajayi@gmail.com>
2018-10-15 20:35:37 +0100

    Created Server and package.json

---
+++ app.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+app.get('/', (req, res) => res.status(200).send('Welcome'));
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
